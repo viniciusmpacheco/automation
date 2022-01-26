5a8db54d45585411e1bc5bcba1aba948b9e64b10
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-01-26 13:20:08 -0300

    Creation of scripting for Node installation automation

---
+++ config/tool/node-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Creation Date: 25/01/2022
+# Author: Lucas Moraes da Silva
+# E-mail: lucasmoraessilva01@gmail.com
+# Remote Rrepository: https://github.com/viniciusmpacheco/automation/
+# Description: Installs the latest LTS version of Node through a script officially available from Nodesource.
+# Run Information: Run the machine preparation script provided and install Node in the latest LTS version. If the script has already been run before, the data entered will be deleted and updated information will be inserted automatically.
+# Log Directory:
+# Use Exemples: 
+#       ./automation/config/tool/node-install.sh
+
+# Imports
+# ----------------------------------------------------------------------------------------------------------------
+
+# Variáveis
+# ----------------------------------------------------------------------------------------------------------------
+
+NODE_SCRIPT_DOWNLOAD_URL="https://deb.nodesource.com/setup_lts.x"
+
+# Functions
+# ----------------------------------------------------------------------------------------------------------------
+
+function main(){
+    echo "Preconfiguring machine for Node installation"
+    # Preconfiguring machine for Node installation
+    executarScriptDePreConfiguracaoDoNode
+
+    echo "Installing the latest LTS version of Node"
+    # Installing the latest LTS version of Node
+    instalarNode
+}
+
+function executarScriptDePreConfiguracaoDoNode(){
+    # Getting script through URL and running it as super user in bash
+    echo "Getting script through URL and running it as super user in bash"
+    curl -fsSL $NODE_SCRIPT_DOWNLOAD_URL | sudo -E bash -
+}
+
+function instalarNode(){
+    # Updating the apt package index and installing the latest LTS version of Node
+    echo "Updating the apt package index and installing the latest LTS version of Node"
+    apt-get update
+    apt-get install -y nodejs
+}
+
+# Execution
+# ----------------------------------------------------------------------------------------------------------------
+
+main
